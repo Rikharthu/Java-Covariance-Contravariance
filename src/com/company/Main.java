4299032b0d8ea433e4809f8130925d01d6292d0c
Rikharthu <uberviolence@gmail.com>
2017-08-13 22:41:06 +0300

    more comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,10 @@ public class Main {
 	// write your code here
         // https://stackoverflow.com/questions/2723397/what-is-pecs-producer-extends-consumer-super
         // Contravariance
-        // Можем положить любой супер-класс
+        // Можем положить любой класс - наследник (Класс, чей родитель является X)
+        // Collection of a definite type which is a super of XYZ
+        // That means we can allways add an instance of XYZ and above
+        // Since we do not know actual type, we cannot retrivie anything from it except Object (WRITE-ONLY collection)
         List<? super EnergySource> energyConsumer = new ArrayList<>();
         List<? super Vegetable> vegetableConsumer = new ArrayList<>();
         List<? super Bamboo> bambooConsumer = new ArrayList<>();
@@ -42,7 +45,11 @@ public class Main {
         bamboo=bambooConsumer.get(0);
 
         // Covariance
-        // Можем достать л
+        // Можем достать любой класс-наследник)
+        // Every object in a given collection implements/extends XYZ
+        // Thus ? is any subclass of XYZ
+        // That means I can get objects out of that list so long as I only need to use the XYZ interface
+        // I can't put objects (except null) since type can't be sure at compile-time (READ-ONLY collection)
         List<? extends EnergySource> energyProducer = new ArrayList<>();
         List<? extends Vegetable> vegetableProducer = new ArrayList<>();
         List<? extends Bamboo> bambooProducer = new ArrayList<>();
@@ -67,12 +74,12 @@ public class Main {
 //        vegetable=energyProducer.get(0);
 //        bamboo=energyProducer.get(0);
 
-        object = energyProducer.get(0);
+        object = vegetableProducer.get(0);
         energy=vegetableProducer.get(0);
         vegetable=vegetableProducer.get(0);
 //        bamboo=vegetableProducer.get(0);
 
-        object = energyProducer.get(0);
+        object = bambooProducer.get(0);
         energy=bambooProducer.get(0);
         vegetable=bambooProducer.get(0);
         bamboo=bambooProducer.get(0);
